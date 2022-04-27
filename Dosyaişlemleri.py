5c0845672d5db3e5b81e7656827402be8920deb5
sahin-emir <93335449+sahin-emir@users.noreply.github.com>
2022-04-27 18:53:58 +0300

    Update Dosyaişlemleri.py

--- Dosyaişlemleri.py
+++ Dosyaişlemleri.py
@@ -10,3 +10,29 @@ import os
 #    return oku
 
 #dosyaoku("ilkdsosya.txt")
+
+## dosya=open("C:\\Users\\Beykent\\Desktop\\deneme\\a1.docx","w")farklı dosyada açma
+
+os.getcwd()
+
+    # listedeki elemanları yazdırma
+#dosya=open("beykenty2.txt","w")
+#liste=["ayva\n","elma\n"]
+
+#dosya.writelines(liste)
+##for i in liste:
+##    dosya.write(i)
+
+
+        # dosyaya yazı yazma 
+#dosya=open("beykent.txt","w")
+#dosya.write("Gerekli yazılar buyraya yazılcak ")
+#dosya.write("\ Gerekli yazılar buyrayaiii yazılcak ")
+
+os.remove("ilkdsosya.txt")
+try:
+    dosya=open("beykenty2.txt")
+    print(dosya.read())
+    print(dosya)
+except:
+    print("dosya bulunamadı")
