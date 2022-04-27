9237a55c254c22ba648baa3dbe3964c293161cb3
sahin-emir <93335449+sahin-emir@users.noreply.github.com>
2022-04-27 18:25:56 +0300

    Create Python_Try,Except.py

---
+++ Python_Try,Except.py
@@ -0,0 +1,36 @@
+## Exception
+
+
+### try exceot basit örnek 
+#def karekok(girdi):
+#    try:
+#        return girdi*girdi
+#    except:
+#        print("sayı sayısal olmalıdır")
+
+#print(karekok(16))
+#print(karekok("iki"))
+
+
+#def bol(sayi1,sayi2):
+#    try:
+#        return sayi1/sayi2
+#    except :
+#        print("sayı2 sayısal olmalıdır")
+#print(bol(5,0))
+#print(bol(10,8))
+
+## Dosya İşlemleri
+
+import os 
+
+#os.getcwd()
+## dosyanın bulunduğu klassörde çalışacak 
+  
+#dosyagir=open("ilkdsosya.txt","w")
+
+#def dosyaoku(isim):
+#    oku=open(isim,"r")
+#    return oku
+
+#dosyaoku("ilkdsosya.txt")
