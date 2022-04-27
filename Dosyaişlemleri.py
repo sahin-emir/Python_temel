07eb1567638d1f8e8b2ba8080dbf6eda95f5c795
sahin-emir <93335449+sahin-emir@users.noreply.github.com>
2022-04-27 18:26:22 +0300

    Create Dosyaişlemleri.py

---
+++ Dosyaişlemleri.py
@@ -0,0 +1,12 @@
+import os 
+
+#os.getcwd()
+## dosyanın bulunduğu klassörde çalışacak 
+  
+#dosyagir=open("ilkdsosya.txt","w")
+
+#def dosyaoku(isim):
+#    oku=open(isim,"r")
+#    return oku
+
+#dosyaoku("ilkdsosya.txt")
