fce4d5eb3c0799f5d8700869f2ae963a8e1824f4
sahin-emir <93335449+sahin-emir@users.noreply.github.com>
2022-04-27 18:26:38 +0300

    Update Python_Try,Except.py

--- Python_Try,Except.py
+++ Python_Try,Except.py
@@ -22,15 +22,4 @@
 
 ## Dosya İşlemleri
 
-import os 
 
-#os.getcwd()
-## dosyanın bulunduğu klassörde çalışacak 
-  
-#dosyagir=open("ilkdsosya.txt","w")
-
-#def dosyaoku(isim):
-#    oku=open(isim,"r")
-#    return oku
-
-#dosyaoku("ilkdsosya.txt")
