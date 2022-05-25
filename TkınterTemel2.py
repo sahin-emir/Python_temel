b7e4542699dfa95205d7e2c08183f6dcbcb43e74
sahin-emir <93335449+sahin-emir@users.noreply.github.com>
2022-05-25 18:41:56 +0300

    Create TkınterTemel2.py

---
+++ TkınterTemel2.py
@@ -0,0 +1,49 @@
+#from tkinter import *
+#from tkinter.ttk import*
+#from tkinter import messagebox
+
+
+#pencere=Tk()
+#pencere.title("RAdıo Button")
+#secili=IntVar()
+#r1=Radiobutton(pencere,text="Bir",value=1,variable=secili)
+#r2=Radiobutton(pencere,text="İki",value=2,variable=secili)
+#r3=Radiobutton(pencere,text="Üç",value=3,variable=secili)
+
+#def tikla():
+#    print(secili.get())
+#buton=Button(pencere,text="Tıkla",command=tikla)
+
+
+#r1.grid(column=0,row=0)
+#r2.grid(column=1,row=0)
+#r3.grid(column=2,row=0)
+#buton.grid(column=3,row=0)
+
+##### Message box
+
+
+#def tikla():
+#    messagebox.showerror("başlık","içerik")
+
+
+#buton=Button(pencere,text="Tıkla",command=tikla)
+#buton.grid(column=0,row=0)
+
+#stil=ttk.Style()
+#stil.theme_use("default")
+#stil.configure("black.Horizontal.TProgressbar",backgroun="black")
+
+#bar=Progressbar(pencere,length=200,style="black.Horizontal.TProgressbar")
+
+#bar["value"]=70
+#bar.grid(column=0,row=0)
+
+#def artir():
+#    bar["value"]+=1
+#buton=Button(ppencere,text="artır",command=artir)
+#bar.grid(column=0,row=0)
+#buton.grid(column=2,row=0)
+
+
+#pencere.mainloop()
